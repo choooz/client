c89a0c5ddfdf9ed2e65cdc07d85f23ed3fa4af50
leejha <jhl2619@naver.com>
2022-10-31 18:10:37 +0900

    feat: 백엔드와 api 통신 확인

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import axios from 'axios';
 
 export default function Home() {
+  const test = async () => {
+    const response = await axios.post('https://manyuser.tk/api/user/signup', {
+      name: 'self',
+      email: 'test@gamil.com',
+      password: 'react',
+    });
+    console.log(response);
+  };
+  test();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -67,5 +78,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
