f4600d33956c4b4a56a2c3ef9212da932fb8ddef
leejiho9898 <cctv1998@naver.com>
2022-12-22 01:23:52 +0900

    feat: assets 이미지 추가

--- apps/web/assets/index.ts
+++ apps/web/assets/index.ts
@@ -2,3 +2,5 @@ export { default as LogoWhite } from "./logo-white.png";
 export { default as PurpleMonster } from "./purple-monster.png";
 export { default as Male } from "./male.png";
 export { default as Female } from "./female.png";
+export { default as CheckRound } from "./check-round.png";
+export { default as Eyes } from "./eyes.png";
