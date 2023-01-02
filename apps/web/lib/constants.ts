727a33bdf442b0e1e6dc263f6c7fb3fb25375cdf
leejha <jhl2619@naver.com>
2023-01-03 01:33:37 +0900

    feat: server url, social client id 상수화

---
+++ apps/web/lib/constants.ts
@@ -0,0 +1,3 @@
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "";
+export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID || "";
+export const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID || "";
