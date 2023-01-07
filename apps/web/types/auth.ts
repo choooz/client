73ea9d62dda022dd8d3a19fbf9537df3784abd38
leejha <jhl2619@naver.com>
2023-01-07 09:44:16 +0900

    feat: schema 타입 분리

--- apps/web/types/auth.ts
+++ apps/web/types/auth.ts
@@ -1,1 +1,17 @@
-export interface UserModel {}
+export interface MBTIType {
+  M: "E" | "I" | null;
+  B: "S" | "N" | null;
+  T: "T" | "F" | null;
+  I: "J" | "P" | null;
+}
+
+export enum Gender {
+  MALE = "male",
+  FEMALE = "female",
+}
+
+export interface UserModel {
+  gender: "male" | "female" | null;
+  MBTI: MBTIType;
+  age: number | null;
+}
