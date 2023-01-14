94e39cd86073d9bec2d1fa902b376028bde32cb6
leejha <jhl2619@naver.com>
2023-01-14 23:59:29 +0900

    feat: access token를 local storage에 저장하기 위한 코드 추가

---
+++ apps/web/lib/utils/userStorage.ts
@@ -0,0 +1,23 @@
+export const USER_STORAGE_KEY = "ACCESS_TOKEN";
+
+const userStorage = {
+  get() {
+    const user = localStorage.getItem(USER_STORAGE_KEY);
+    try {
+      if (!user) return null;
+      const parsedUser = JSON.parse(user);
+      return parsedUser;
+    } catch (e) {
+      localStorage.removeItem(USER_STORAGE_KEY);
+      return null;
+    }
+  },
+  set(user: any) {
+    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+  },
+  remove() {
+    localStorage.removeItem(USER_STORAGE_KEY);
+  },
+};
+
+export default userStorage;
