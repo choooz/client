471774c48440f2321e17794d425624a0f74a6b62
leejha <jhl2619@naver.com>
2023-01-16 19:47:54 +0900

    feat: redirect url constants.ts에 추가

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,7 +1,18 @@
+import Path from "./Path";
+
 export const CLIENT_URL = "https://client-qm8u5r52v-fashion-teacher.vercel.app/";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "";
+
 export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID || "";
 export const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID || "";
+export const KAKAO_LOGIN_REDIRECT_URL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:3000/${Path.KAKAO_LOGIN_PROCESS}`
+    : `${CLIENT_URL}${Path.KAKAO_LOGIN_PROCESS}`;
+export const NAVER_LOGIN_REDIRECT_URL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:3000/${Path.NAVER_LOGIN_PROCESS}`
+    : `${CLIENT_URL}${Path.NAVER_LOGIN_PROCESS}`;
 
 export const AGE_LIST = [
   { id: "10", name: "10대" },
