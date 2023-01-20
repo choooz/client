b04ddc3aa932b97c76a206b4c6271b2516f13c60
leejiho9898 <cctv1998@naver.com>
2023-01-20 15:38:22 +0900

    feat: 투표 생성 모달 퍼블리싱

--- package/ui/components/divide/DivideLine.tsx
+++ package/ui/components/divide/DivideLine.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 function DivideLine() {
-  return <Line>DivideLine</Line>;
+  return <Line />;
 }
 
 const Line = styled.div`
   width: 100%;
   height: 1px;
   background-color: ${({ theme }) => theme.palette.border.base};
+  margin: 24px 0;
 `;
 
 export default DivideLine;
