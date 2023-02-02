5da3acd2b6c56fa6579b7ef671da30b401c9c3df
leejiho9898 <cctv1998@naver.com>
2023-02-02 18:04:06 +0900

    refactor: dom 조작하는 코드 삭제

--- apps/web/components/select/hooks/useFlipAnimation.ts
+++ apps/web/components/select/hooks/useFlipAnimation.ts
@@ -1,40 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
+export type Drag = "up" | "down" | null;
 function useFlipAnimation() {
+  const [drag, setDrag] = useState<Drag>(null);
   let timer: NodeJS.Timeout;
   const onAniamteFlip = (e: React.WheelEvent<HTMLDivElement>) => {
-    // 휠을 올리면 className을 up로 변경하고, 1초뒤 다시 원래대로 변경
-    if (e.deltaY < 0) {
-      document.querySelector(".animate")?.classList.add("up");
-      document.querySelector(".animate2")?.classList.add("up2");
-      document.querySelector(".animate3")?.classList.add("up3");
-
-      timer = setTimeout(() => {
-        document.querySelector(".animate")?.classList.remove("up");
-        document.querySelector(".animate2")?.classList.remove("up2");
-        document.querySelector(".animate3")?.classList.remove("up3");
-      }, 1500);
-    }
-
-    //  휠을 아래로 내리면 className을 prev로 변경하고, 1초뒤 다시 원래대로 변경
-    if (e.deltaY > 0) {
-      document.querySelector(".animate")?.classList.add("down");
-      document.querySelector(".animate2")?.classList.add("down2");
-
-      timer = setTimeout(() => {
-        document.querySelector(".animate")?.classList.remove("down");
-        document.querySelector(".animate2")?.classList.remove("down2");
-      }, 1500);
-    }
+    // 휠을 올리면 up, 내리면 down
+    if (e.deltaY < 0) setDrag("up");
+    if (e.deltaY > 0) setDrag("down");
+    timer = setTimeout(() => {
+      setDrag(null);
+    }, 1500);
   };
-
-  useEffect(() => {
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
-  return { onAniamteFlip };
+  return { onAniamteFlip, drag };
 }
 
 export default useFlipAnimation;
