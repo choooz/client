05cd4ebb36076e079cb9dbb81227d6fe3350db78
leejha <jhl2619@naver.com>
2023-02-10 03:07:31 +0900

    feat: 투표 타입 추가

--- apps/web/types/vote.ts
+++ apps/web/types/vote.ts
@@ -3,3 +3,18 @@ export type CategoryNameType = "FOODS" | "CAREER" | "LOVE" | "FASHION" | "INTERE
 export type Direction = "left" | "right";
 
 export type ActiveType = "active" | "inactive" | null;
+
+export interface Writer {
+  imageUrl: string;
+  nickname: string;
+  userid: number;
+}
+export interface Vote {
+  voteId: number;
+  category: CategoryNameType;
+  totalTitle: string;
+  writer: Writer;
+  filteredAge: number;
+  filteredGender: boolean;
+  filteredMbti: string;
+}
