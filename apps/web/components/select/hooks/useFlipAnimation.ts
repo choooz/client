bfa07d5e3a5cf1e7e27042cc7ce11811b80b053e
leejiho9898 <cctv1998@naver.com>
2023-02-15 06:17:17 +0900

    feat: 숏폼 기능 구현 훅, 무한 데이터 fetch 서비스 구현

--- apps/web/components/select/hooks/useFlipAnimation.ts
+++ apps/web/components/select/hooks/useFlipAnimation.ts
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
 export type Drag = "up" | "down" | null;
-function useFlipAnimation() {
+function useFlipAnimation(onChangeNowShowing: (index: number) => void) {
   const [drag, setDrag] = useState<Drag>(null);
-  let timer: NodeJS.Timeout;
-  const onAniamteFlip = (e: React.WheelEvent<HTMLDivElement>) => {
+
+  const onActFlip = (e: React.WheelEvent<HTMLDivElement>) => {
     // 휠을 올리면 up, 내리면 down
-    if (e.deltaY < 0) setDrag("up");
-    if (e.deltaY > 0) setDrag("down");
-    timer = setTimeout(() => {
+    if (e.deltaY < 0) {
+      setDrag("up");
+      setTimeout(() => {
+        onChangeNowShowing(-1);
+      }, 750);
+    }
+    if (e.deltaY > 0) {
+      setDrag("down");
+      setTimeout(() => {
+        onChangeNowShowing(1);
+      }, 750);
+    }
+
+    setTimeout(() => {
       setDrag(null);
-    }, 1500);
+    }, 850);
   };
-  return { onAniamteFlip, drag };
+  return { onActFlip, drag };
 }
 
 export default useFlipAnimation;
---
+++ apps/web/services/useInfiniteMainListService.ts
@@ -0,0 +1,38 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getVoteListAPI } from "lib/apis/vote";
+import { reactQueryKeys } from "lib/queryKeys";
+import { useEffect, useMemo, useState } from "react";
+
+export default function useInfiniteMainListService(size: number, sortBy: string) {
+  const [nowShowing, setNowShowing] = useState(0);
+
+  const { data, isLoading, isError, fetchNextPage } = useInfiniteQuery(
+    reactQueryKeys.mainVoteList(),
+    ({ pageParam = 0 }) => getVoteListAPI({ page: pageParam, size, sortBy }),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.last) return undefined;
+        return pages.length + 1;
+      },
+      keepPreviousData: true,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+
+  const mainVoteList = useMemo(
+    () => (data ? data.pages.flatMap((page) => page.content) : []),
+    [data],
+  );
+
+  const onChangeNowShowing = (index: number) => {
+    if (nowShowing + index < 0) return;
+    if (nowShowing + index > mainVoteList.length) return;
+    setNowShowing((prev) => prev + index);
+  };
+
+  useEffect(() => {
+    if (nowShowing === mainVoteList.length - 1) fetchNextPage();
+  }, [nowShowing, mainVoteList.length]);
+
+  return { data, isLoading, isError, mainVoteList, nowShowing, onChangeNowShowing };
+}
