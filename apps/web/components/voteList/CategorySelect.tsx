e29c40206f4e1bb5b8a654ff3fea41f82a4f49ea
leejha <jhl2619@naver.com>
2023-02-19 02:11:32 +0900

    feat: select 리팩터링 반영 후  최신순 정렬 박스 형태 수정

--- apps/web/components/voteList/CategorySelect.tsx
+++ apps/web/components/voteList/CategorySelect.tsx
@@ -1,6 +1,5 @@
 import { CATEGORY_LIST } from "lib/constants";
 import { Select } from "@chooz/ui";
-import { SelectIndicator } from "public/icons";
 import styled, { css } from "styled-components";
 import { media } from "styles/media";
 
@@ -23,40 +22,16 @@ function CategorySelect({ categoryOption, onChangeCategoryOption }: Props) {
   );
 }
 
-const SelectStyled = styled.span`
-  ${({ theme }) => css`
+const SelectStyled = styled.div`
+  width: 104px;
+  ${({ theme }) => `
+    ${theme.textStyle.Title_Large};
     color: ${theme.palette.ink.dark};
-    ${theme.textStyle.Title_3};
-    ${media.medium} {
-      ${theme.textStyle.Title_Large};
-    }
-    button {
-      display: flex;
-      justify-content: space-around;
-      > span {
-        margin-left: 8px;
-        flex-direction: row;
-        align-items: center;
+    .selected-text-container {
         font-weight: 700;
       }
-    }
-    ul {
-      position: absolute;
-      margin-top: 8px;
-      border-radius: 8px;
-      box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.1);
-      z-index: 99;
-      border: solid 1px ${theme.palette.border.base};
-      background-color: ${theme.palette.background.white};
-    }
-    li {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 14px 34px;
-      :hover {
-        background-color: ${theme.palette.background.selectedSoft};
-      }
+    ${media.medium} {
+      ${theme.textStyle.Title_3};
     }
   `}
 `;
