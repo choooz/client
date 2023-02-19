8cddd1bbcc947a11aee054eca9aca9eb969ea271
Leejha <57664427+Leejha@users.noreply.github.com>
2023-02-19 10:21:20 +0900

    [CZ-417] 정렬 셀렉트 박스 테두리 수정 (#62)
    * refactor: select 컴포넌트 리팩터링
    
    * feat: select 리팩터링 반영 후  최신순 정렬 박스 형태 수정
    
    * feat: 고유한 아이디를 만들어야 하지만 당장 생각이 안나 todo 추가

--- packages/ui/components/selectBox/OptionList.tsx
+++ packages/ui/components/selectBox/OptionList.tsx
@@ -1,3 +1,4 @@
+import styled, { css } from "styled-components";
 import Option from "./Option";
 
 interface Option {
@@ -13,7 +14,8 @@ interface Props {
 
 function OptionList({ options, onChangeSelectedOption, onToggleOpen }: Props) {
   return (
-    <ul aria-labelledby="select-box-1" role="listbox">
+    // @todo 고유한 id를 만들어야함
+    <Ul id="select-list" aria-labelledby="select-box-1" role="listbox">
       {options.map(({ value, label }) => (
         <Option
           key={`select_${value}`}
@@ -24,8 +26,21 @@ function OptionList({ options, onChangeSelectedOption, onToggleOpen }: Props) {
           }}
         />
       ))}
-    </ul>
+    </Ul>
   );
 }
+
+const Ul = styled.ul`
+  position: absolute;
+  margin-top: 8px;
+  border-radius: 8px;
+  box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.1);
+  z-index: 99;
+  ${({ theme }) =>
+    css`
+      border: solid 1px ${theme.palette.border.base};
+      background-color: ${theme.palette.background.white};
+    `}
+`;
 
 export default OptionList;
