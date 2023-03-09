084a5589a2d71c6ee5f7b80d82c45c45a5df1da6
leejiho9898 <cctv1998@naver.com>
2023-03-09 20:01:20 +0900

    feat: 투표, 투표여부 불러오는 service 생성

--- apps/web/services/useMutateVotingService.ts
+++ apps/web/services/useMutateVotingService.ts
@@ -1,16 +1,29 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
-import { postVotingById } from "lib/apis/voting";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getVotingCheck, postVotingById } from "lib/apis/voting";
+import { reactQueryKeys } from "lib/queryKeys";
 
 import { useState } from "react";
 import { AorB, Voting } from "types/vote";
 
 export default function useMutateVotingService(voteId: number) {
   const [select, setSelect] = useState<Voting>({ choice: null });
-  const onChangeSelect = (select: AorB) => {
+
+  const { mutate } = useMutation((choice: AorB) => postVotingById(voteId, { choice }));
+
+  const onMutateVoting = (select: AorB) => {
     setSelect({ choice: select });
+    mutate(select);
   };
 
-  const { mutate } = useMutation(() => postVotingById(voteId, select));
+  const { data } = useQuery(reactQueryKeys.votingCheck(voteId), () => getVotingCheck(voteId), {
+    onSuccess: (data) => {
+      if (data.voted) {
+        setSelect({ choice: data.userChoice });
+      } else setSelect({ choice: null });
+    },
+    cacheTime: 0,
+    staleTime: 0,
+  });
 
-  return { mutate, onChangeSelect, select };
+  return { mutate, onMutateVoting, select, data };
 }
