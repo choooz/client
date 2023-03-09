56ad252f27a63c3e6ef8a810df6673b35046069f
leejiho9898 <cctv1998@naver.com>
2023-03-09 15:54:03 +0900

    fix: 중복되는 api 제거

--- apps/web/lib/apis/voting.ts
+++ apps/web/lib/apis/voting.ts
@@ -1,10 +1,12 @@
+import { AorB } from "types/vote";
 import apiClient from "./apiClient";
 
 interface PostVotingRequest {
-  choice: "A";
+  choice: AorB | null;
 }
 
 export const postVotingById = async (voteId: number, body: PostVotingRequest) => {
+  if (body.choice === null) throw new Error("A와 B중 하나를 선택해주세요.");
   const response = await apiClient.post(`api/votes/${voteId}/vote`, body);
   return response.data;
 };
--- apps/web/services/useMutateVotingService.ts
+++ apps/web/services/useMutateVotingService.ts
@@ -1,20 +1,16 @@
 import { QueryClient, useMutation } from "@tanstack/react-query";
-import { postVotingAPI } from "lib/apis/vote";
+import { postVotingById } from "lib/apis/voting";
+
 import { useState } from "react";
 import { AorB, Voting } from "types/vote";
 
 export default function useMutateVotingService(voteId: number) {
-  const queryClient = new QueryClient();
   const [select, setSelect] = useState<Voting>({ choice: null });
   const onChangeSelect = (select: AorB) => {
     setSelect({ choice: select });
   };
 
-  const { mutate } = useMutation(() => postVotingAPI(select, voteId), {
-    onSuccess: () => {
-      // 이곳에 통계 데이터를 refetch하는 로직을 넣어야 합니다.
-    },
-  });
+  const { mutate } = useMutation(() => postVotingById(voteId, select));
 
   return { mutate, onChangeSelect, select };
 }
