742a894fc91576a9a978c6816a6a37ab07873b19
leejiho9898 <cctv1998@naver.com>
2023-03-16 19:12:16 +0900

    feat: 댓글 작성 후 성공시 댓글 내용 초기화 시키는 로직 구현

--- apps/web/components/detail/CommentContainer.tsx
+++ apps/web/components/detail/CommentContainer.tsx
@@ -32,7 +32,7 @@ function CommentContainer({ postId }: Props) {
     {
       getNextPageParam: (lastPage, pages) => {
         // @NOTE 백엔드에서 last 작동이 안되어 주석
-        // if (lastPage.last) return undefined;
+        if (lastPage.last) return undefined;
         return pages.length + 1;
       },
       keepPreviousData: true,
