7de2ea3fd2522357e6672bd30d0a8622e743ea37
leejha <jhl2619@naver.com>
2023-03-19 00:14:22 +0900

    feat: 검색어 추천 api 추가

--- apps/web/types/vote.ts
+++ apps/web/types/vote.ts
@@ -1,5 +1,6 @@
 import { PostVotingRequest } from "lib/apis/vote";
 
+// @Todo ui타입과 api타입을 분리해야할 것 같다. null값 땜시
 export type CategoryNameType = "FOODS" | "CAREER" | "LOVE" | "FASHION" | "INTEREST" | "NULL";
 
 export type Direction = "left" | "right";
