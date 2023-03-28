28ee214c9cde4ffd3ff7674e8348c102856acf9b
leejiho9898 <cctv1998@naver.com>
2023-03-28 23:35:37 +0900

    fix: 필요없는 로직 삭제

--- apps/web/services/useFilteredStatisticsService.ts
+++ apps/web/services/useFilteredStatisticsService.ts
@@ -4,13 +4,13 @@ import { reactQueryKeys } from "lib/queryKeys";
 
 export default function useFilteredStatisticsService(
   voteId: number,
-  gender: string | null,
-  mbti: string | null,
-  age: string | null,
+  gender: string,
+  mbti: string,
+  age: string,
 ) {
   const voteStatisticsQuery = useQuery(
-    reactQueryKeys.detailFilterdAnalysis(voteId, gender || "", mbti || "", age || ""),
-    () => getFilterdStatisticsById(voteId, gender || "", mbti || "", age || ""),
+    reactQueryKeys.detailFilterdAnalysis(voteId, gender, mbti, age),
+    () => getFilterdStatisticsById(voteId, gender, mbti, age),
     {
       enabled: !!voteId,
     },
