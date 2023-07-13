56203c76e6ea994cac6137d50c5cc2631bc6b335
이재하 <jhl2619@naver.com>
2023-07-13 19:16:34 +0900

    fix: 위로 드래그시 이전 투표를 보여주도록 수정

--- apps/web/app/select/hooks/useFlipAnimation.ts
+++ apps/web/app/select/hooks/useFlipAnimation.ts
@@ -35,16 +35,17 @@ function useFlipAnimation(onChangeNowShowing: (index: number) => void) {
   const onTouchMoveActFlip = (e: React.TouchEvent<HTMLDivElement>) => {
     const now = Date.now();
     const timeDiff = now - lastTouchEventTimeRef.current;
-    if (timeDiff < 1000) {
-      // 이전 이벤트가 1초 내에 발생한 경우 무시
+    if (timeDiff < 1500) {
+      // 이전 이벤트가 1.5초 내에 발생한 경우 무시
       return;
     }
     lastTouchEventTimeRef.current = now;
     //터치의 시작이 끝보다 위면 up, 아래면 down
+
     if (e.touches[0].clientY < e.changedTouches[0].clientY + 300) {
       setDrag("up");
       setTimeout(() => {
-        onChangeNowShowing(1);
+        onChangeNowShowing(-1);
       }, 750);
     }
     if (e.touches[0].clientY > e.changedTouches[0].clientY - 300) {
