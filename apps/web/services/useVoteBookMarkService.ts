6b01a556fd929d246dba1bab9da345d0ebd5f16d
leejiho9898 <cctv1998@naver.com>
2023-07-30 20:23:58 +0900

    feat 북마크 에러 로그인 추가

--- apps/web/services/useVoteBookMarkService.ts
+++ apps/web/services/useVoteBookMarkService.ts
@@ -1,9 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookMarkCheckAPI, postBookmarkAPI } from "lib/apis/vote";
+import Path from "lib/Path";
 import { queryKeys, reactQueryKeys } from "lib/queryKeys";
+import { useRouter } from "next/navigation";
 
 export default function useBookMarkService(voteId: number) {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const bookMarkCheckQuery = useQuery(
     reactQueryKeys.bookmarkCheck(voteId),
@@ -16,6 +19,11 @@ export default function useBookMarkService(voteId: number) {
   const { mutate: mutateBookMark } = useMutation(() => postBookmarkAPI(voteId), {
     onSuccess: () => {
       queryClient.invalidateQueries([queryKeys.BOOKMARK_CHECK]);
+    },
+    onError: () => {
+      if (confirm("로그인이 필요한 서비스입니다.")) {
+        router.push(Path.LOGIN_PAGE);
+      }
     },
   });
 
