99739862eee64eb2c3b1a6f14b30e12a45e8f12d
이재하 <jhl2619@naver.com>
2023-08-02 01:29:49 +0900

    fix: Image 태그에 width, height 속성 추가

--- apps/web/components/detail/CommentForm.tsx
+++ apps/web/components/detail/CommentForm.tsx
@@ -23,9 +23,9 @@ function CommentForm({ commentForm, onChangeCommentForm, onSubmitComment, profil
       <Image
         src={profileImage || PurpleMonster}
         alt="댓글 프로필"
+        width={40}
+        height={40}
         style={{
-          width: "40px",
-          height: "40px",
           borderRadius: "8px",
           marginRight: "10px",
         }}
