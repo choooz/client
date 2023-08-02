8a652e69ca949d42fcb2083aad6c63197f2fde2c
이재하 <jhl2619@naver.com>
2023-08-02 23:28:02 +0900

    refactor: 프로필 편집 페이지에서 비즈니스 로직과 ui 분리

--- apps/web/app/my/edit/components/UserInfoEditContainer.tsx
+++ apps/web/app/my/edit/components/UserInfoEditContainer.tsx
@@ -1,69 +1,24 @@
 import styled, { css } from "styled-components";
-import { Button, transitions } from "@chooz/ui";
+import { Button } from "@chooz/ui";
 import MbtiSelect from "app/my/components/MbtiSelect";
-import { useGetUserInfo } from "hooks/useGetUserInfo";
-import { uploadProfileImageAPI } from "lib/apis/upload";
-import Path from "lib/Path";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { CheckRound } from "public/images";
-import useRegisterService from "services/useRegisterService";
+
 import { media } from "styles/media";
 import { Gender } from "types/user";
-import { IMAGE_CATEGORY_LIST } from "types/vote";
-import { updateUserInfo } from "lib/apis/my";
 import ImageUpload from "./ImageUpload";
+import CategoryList from "./CategoryList";
+import useEditProfileService from "../services/useEditProfileService";
 
 function UserInfoEditContainer() {
-  const router = useRouter();
-
-  const { categoryLists, onClickCategory } = useRegisterService();
-
-  // @Todo useQuery 상태 값을 잘 사용할 수 있는 방법이 없을까
-  const { userInfo, setUserInfo } = useGetUserInfo();
-
-  if (!userInfo) return <></>;
-
+  const {
+    userInfo,
+    categoryList,
+    onClickCategory,
+    onChangeUsername,
+    onChangeMbti,
+    onClickUpdateUserInfo,
+    onUploadImage,
+  } = useEditProfileService();
   const { gender, username, age, mbti, imageUrl } = userInfo;
-  const onChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserInfo({ ...userInfo, username: e.target.value });
-  };
-
-  const onChangeMbti = (value: string) => {
-    setUserInfo({ ...userInfo, mbti: value });
-  };
-
-  const onClickUpdateUserInfo = () => {
-    try {
-      updateUserInfo({
-        nickname: username,
-        mbti,
-        categoryList: categoryLists,
-        image: imageUrl,
-      });
-      router.push(Path.MY_PAGE);
-    } catch (error) {
-      alert("MBTI 수정 후 2개월 내에 수정할 수 없습니다.");
-    }
-  };
-
-  const onUploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files === null) return;
-    // if (e.target.files[0].size > 10000000 || e.target.files[1].size > 10000000) {
-    //   alert("파일 용량이 10MB를 초과하였습니다.");
-    //   return;
-    // }
-
-    const formData = new FormData();
-    formData.append("images", e.target.files[0]);
-    try {
-      const data = await uploadProfileImageAPI(formData);
-      setUserInfo({ ...userInfo, imageUrl: data.imageUrl });
-    } catch (error) {
-      alert("이미지 업로드에 실패했습니다." + error);
-    }
-    return;
-  };
 
   return (
     <PageInner>
@@ -82,30 +37,7 @@ function UserInfoEditContainer() {
       <MbtiSelect mbtiOption={mbti} onChangeMbti={onChangeMbti}></MbtiSelect>
       <WarningMessage>MBTI 수정시 2개월간 바꿀 수 없습니다.</WarningMessage>
       <UserInfoTitle>카테고리</UserInfoTitle>
-      <CategoryContainer>
-        {/* @Todo 컴포넌트화 하기 */}
-        {IMAGE_CATEGORY_LIST.map(({ image, value, label }) => (
-          <Category
-            width="49%"
-            height="104px"
-            borderRadius="10px"
-            key={`profile_edit_page_${value}`}
-            selected={categoryLists.includes(value)}
-            onClick={onClickCategory}
-            name={value}
-          >
-            <Image alt="항목" src={image} height={32} />
-            <CategoryText>
-              {categoryLists.includes(value) && (
-                <CheckBoxWrapper>
-                  <Image alt="선택" src={CheckRound} width={16} />
-                </CheckBoxWrapper>
-              )}
-              {label}
-            </CategoryText>
-          </Category>
-        ))}
-      </CategoryContainer>
+      <CategoryList categoryList={categoryList} onClickCategory={onClickCategory} />
       <CompleteButton variant="primary" width="100%" height="56px" onClick={onClickUpdateUserInfo}>
         완료
       </CompleteButton>
@@ -174,46 +106,6 @@ const GenderAndAgeInput = styled.input`
 const WarningMessage = styled.div`
   color: ${({ theme }) => theme.palette.system.danger};
   margin-top: 6px;
-`;
-
-const CategoryContainer = styled.div`
-  display: grid;
-  grid-template-columns: 50% 50%;
-  gap: 9px;
-  ${media.medium} {
-    gap: 14px 16px;
-  }
-`;
-
-const Category = styled(Button)<{ selected: boolean }>`
-  position: relative;
-  width: 100%;
-  flex-direction: column;
-  gap: 8px;
-  font-weight: 400;
-  background-color: ${({ theme }) => theme.palette.background.soft};
-  border: 1px solid ${({ theme }) => theme.palette.border.base};
-  ${({ theme }) => theme.textStyle.Title_Small};
-  ${({ theme, selected }) =>
-    selected &&
-    css`
-      animation: ${transitions.blink} 0.7s ease-in-out;
-      background-color: ${theme.palette.background.selected};
-      border: 1px solid ${theme.palette.main.point};
-      font-weight: 700;
-    `}
-`;
-
-const CategoryText = styled.div`
-  align-items: center;
-  display: flex;
-  gap: 4px;
-`;
-
-const CheckBoxWrapper = styled.div`
-  position: absolute;
-  top: 16px;
-  right: 16px;
 `;
 
 const CompleteButton = styled(Button)`
