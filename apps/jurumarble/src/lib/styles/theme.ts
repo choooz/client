d463ae14fbe65b0cbb76165131d73ce7f8ad5abc
이재하 <jhl2619@naver.com>
2023-08-09 22:18:39 +0900

    feat: themProvider 구현

---
+++ apps/jurumarble/src/lib/styles/theme.ts
@@ -0,0 +1,122 @@
+// import { DefaultTheme, Colors } from "styled-components";
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  black_01: "#222222",
+  black_02: "#373737",
+  black_03: "#676767",
+  black_04: "#898989",
+  black_05: "#cccccc",
+  bg_01: "#f0f0f0",
+  bg_02: "#f5f5f5",
+  line_01: "#e9e9e9",
+  line_02: "#f5f5f5",
+  main_01: "#ff4a16",
+  main_02: "#ffe8e1",
+  sub_01: "#ffa183",
+  sub_02: "#aedac4",
+  system_red: "#dc0000",
+  system_green: "#04b014",
+  modal: "rgba(0, 0, 0, 0.5)",
+} as const;
+
+const typography = {
+  headline01: `
+    font-size: 24px; 
+    font-style: normal; 
+    font-weight: 700; 
+    line-height: 130%; 
+    letter-spacing: -0.24px; 
+    `,
+  headline02: `
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+    letter-spacing: -0.2px;
+    `,
+  headline03: `
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%;
+    letter-spacing: -0.18px;
+    `,
+  headline04: `
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+    letter-spacing: -0.16px;
+    `,
+  subhead01: `
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.18px;
+    `,
+  subhead02: `
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.14px;
+    `,
+  body01: `
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+    letter-spacing: -0.16px;
+    `,
+  body_long01: `
+
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+    letter-spacing: -0.16px;
+    `,
+  body02: `
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%
+    letter-spacing: -0.16px;
+    `,
+  body03: `
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.14px;
+    `,
+  body_long03: `
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+    letter-spacing: -0.14px;
+    `,
+  caption: `
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 100%;
+    `,
+  chip: `
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 100%;
+    `,
+} as const;
+
+export type ThemeColors = typeof colors;
+export type ThemeTypography = typeof typography;
+
+export const jurumarbleTheme: DefaultTheme = {
+  colors,
+  typography,
+};
