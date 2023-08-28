a8b746116028ee538d7ba3bdd598714624e054e2
이재하 <jhl2619@naver.com>
2023-08-28 22:33:34 +0900

    feat: 글로벌 스타일에 box-sizing: border-box 추가

--- apps/jurumarble/src/lib/styles/globalStyles.ts
+++ apps/jurumarble/src/lib/styles/globalStyles.ts
@@ -3,6 +3,9 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+    *, :after, :before, ::before, ::after {
+    box-sizing: border-box;
+    }
 `;
 
 export default GlobalStyles;
