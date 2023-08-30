f04d8453f61480af75ea5f60e259597f443305c9
이재하 <jhl2619@naver.com>
2023-08-30 20:24:45 +0900

    feat: 글로벌 스타일 추가

--- apps/jurumarble/src/lib/styles/globalStyles.ts
+++ apps/jurumarble/src/lib/styles/globalStyles.ts
@@ -3,8 +3,24 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+    * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    color: inherit;
+}
     *, :after, :before, ::before, ::after {
     box-sizing: border-box;
+    }
+    ul, ol, li {
+    list-style: none;
+    }
+    button,
+    [role='button'] {
+    border: 0;
+    border-radius: 0;
+    background: none;
+    cursor: pointer;
     }
 `;
 
