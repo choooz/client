a423472e05b61f33dc8a62aa3b74eba7018a0018
이재하 <jhl2619@naver.com>
2023-08-30 20:33:54 +0900

    feat: 선택된 술을 업데이트 할 수 있는 훅 추가

---
+++ apps/jurumarble/src/app/post/hooks/useUpdateSelectedDrinkList.ts
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function useUpdateSelectedDrinkList() {
+  const [selectedDrinkList, setDrinkList] = useState([]);
+
+  const onClickAddDrink = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const name = e.currentTarget.name;
+    selectedDrinkList.includes(name)
+      ? setDrinkList((prev) => prev.filter((item) => item !== name))
+      : selectedDrinkList.length < 2 && setDrinkList((prev) => [...prev.concat(name)]);
+  };
+
+  const onClickDeleteItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const name = e.currentTarget.name;
+
+    setDrinkList((prev) => prev.filter((item) => item !== name));
+  };
+
+  return { selectedDrinkList, onClickAddDrink, onClickDeleteItem };
+}
