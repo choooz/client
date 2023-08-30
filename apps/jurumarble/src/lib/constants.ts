259d8816fd1bad3f14da42f41a86d9df192e5401
이재하 <jhl2619@naver.com>
2023-08-30 20:26:15 +0900

    feat: 지역 리스트 상수 추가

---
+++ apps/jurumarble/src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const REGION_LIST = [
+  { value: "gyeonggiDo", label: "경기도" },
+  { value: "chungcheongDo", label: "충청도" },
+  { value: "gyeongsangDo", label: "경상도" },
+  { value: "gangwonDo", label: "강원도" },
+  { value: "jeollaDo", label: "전라도" },
+  { value: "seoul", label: "서울" },
+  { value: "incheon", label: "인천" },
+  { value: "sejong", label: "세종" },
+  { value: "daejeon", label: "대전" },
+  { value: "busan", label: "부산" },
+  { value: "daegu", label: "대구" },
+  { value: "gwangju", label: "광주" },
+  { value: "ulsan", label: "울산" },
+  { value: "jeju", label: "제주" },
+];
