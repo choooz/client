e3535489b9e220dd4ac7b3aad029e6c4a13dbc82
이재하 <jhl2619@naver.com>
2023-08-31 17:38:12 +0900

    feat: 이미지를 s3에 업로드하는 api 추가

---
+++ apps/jurumarble/src/lib/apis/common.ts
@@ -0,0 +1,12 @@
+import { http } from "./http";
+
+export interface postVoteResponse {
+  imageUrl: string;
+  message: string;
+}
+
+export const uploadImageAPI = async (body: FormData) => {
+  const response = await http.post<postVoteResponse>("api/images", body);
+  // return response.data;
+  return response;
+};
