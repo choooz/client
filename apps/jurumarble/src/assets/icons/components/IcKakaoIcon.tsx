451045f40c6531c4f06913c7cd85b8412234671d
이재하 <jhl2619@naver.com>
2023-09-03 17:33:18 +0900

    feat: 카카오, 네이버 아이콘 svg 컴포넌트 추가

---
+++ apps/jurumarble/src/assets/icons/components/IcKakaoIcon.tsx
@@ -0,0 +1,18 @@
+import type { SVGProps } from "react";
+const SvgKakaoIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 18 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M9 0C4.03081 0 0 2.96597 0 6.61951C0 8.89118 1.55687 10.9 3.93128 12.0931L2.93602 15.5646C2.88626 15.7534 3.00711 15.9421 3.20616 15.9893C3.30569 16.0163 3.41232 15.9893 3.49763 15.9421L7.87678 13.1918C8.24645 13.2255 8.62322 13.2458 9 13.2458C13.9692 13.2458 18 10.2798 18 6.62625C18 2.97271 13.9692 0 9 0Z"
+      fill="currentColor"
+      vectorEffect="non-scaling-stroke"
+    />
+  </svg>
+);
+export default SvgKakaoIcon;
