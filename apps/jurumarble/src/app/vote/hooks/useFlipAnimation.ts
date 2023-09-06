4516249a23de4f6c33ad72b8b6d2f7d8585a0ead
leejiho9898 <cctv1998@naver.com>
2023-09-06 20:57:44 +0900

    feat react import 제거

--- apps/jurumarble/src/app/vote/hooks/useFlipAnimation.ts
+++ apps/jurumarble/src/app/vote/hooks/useFlipAnimation.ts
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 export type Drag = "up" | "down" | null;
 function useFlipAnimation(onChangeNowShowing: (index: number) => void) {
