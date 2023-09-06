7e7c28592ade89158450a2140b94270e8670616f
Leejha <57664427+Leejha@users.noreply.github.com>
2023-09-06 13:07:43 +0900

    [JR-658] 음식 이미지 추가 퍼블리싱  (#138)
    * fix: variantStyles 적용을 위한 우선순위 변경
    
    * feat: 이미지를 추가시 필요한 퍼블리싱 구현

--- apps/jurumarble/src/app/vote/[id]/components/SearchRestaurantModal.tsx
+++ apps/jurumarble/src/app/vote/[id]/components/SearchRestaurantModal.tsx
@@ -3,8 +3,12 @@
 import { Button, ModalTemplate } from "components/index";
 import SearchInput from "components/SearchInput";
 import VoteHeader from "components/VoteHeader";
+import { ThemeColors, transitions } from "lib/styles";
+import Image from "next/image";
+import { EmptyAImg } from "public/images";
+import { useState } from "react";
 import SvgIcX from "src/assets/icons/components/IcX";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import RestaurantItem from "./RestaurantItem";
 
 interface Props {
@@ -15,23 +19,71 @@ const TEMP_LIST = [
   { manufacturer: "gyeonggiDo", drinkName: "경기도" },
   { manufacturer: "chungcheongDo", drinkName: "충청도" },
   { manufacturer: "gyeongsangDo", drinkName: "경상도" },
-  { manufacturer: "gangwonDo", drinkName: "강원도" },
-  { manufacturer: "jeollaDo", drinkName: "전라도" },
-  { manufacturer: "seoul", drinkName: "서울" },
-  { manufacturer: "incheon", drinkName: "인천" },
-  { manufacturer: "sejong", drinkName: "세종" },
-  { manufacturer: "daejeon", drinkName: "대전" },
-  { manufacturer: "busan", drinkName: "부산" },
-  { manufacturer: "daegu", drinkName: "대구" },
-  { manufacturer: "gwangju", drinkName: "광주" },
   { manufacturer: "ulsan", drinkName: "울산" },
   { manufacturer: "jeju", drinkName: "제주" },
 ];
 
 function SearchRestaurantModal({ onToggleSearchRestaurantModal }: Props) {
+  const [selected, setSelected] = useState("");
+  const onClickSelected = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelected(e.currentTarget.name);
+    e.currentTarget.name === "nonSelect" && setSelected("nonSelect");
+  };
   return (
     <ModalTemplate width="375px" height="100%" onToggleModal={onToggleSearchRestaurantModal}>
       <VoteHeader
+        rightButton={
+          <CloseButton onClick={onToggleSearchRestaurantModal}>
+            <SvgIcX width={24} height={24} />
+          </CloseButton>
+        }
+      >
+        <TitleStyled>이미지 선택</TitleStyled>
+      </VoteHeader>
+      <Container>
+        <RestaurantItem />
+        <FoodImageList>
+          <NonSelectedButton
+            variant="outline"
+            width="107px"
+            height="60px"
+            borderRadius="4px"
+            name="nonSelect"
+            onClick={onClickSelected}
+          >
+            <ColorBox selected={selected === "nonSelect"} />
+            선택 안함
+          </NonSelectedButton>
+          {TEMP_LIST.map(({ manufacturer }) => (
+            <FoodItem>
+              <Button key={manufacturer} name={manufacturer} onClick={onClickSelected}>
+                <ColorBox selected={selected === manufacturer} />
+              </Button>
+              <Image
+                alt="음식 이미지"
+                src={EmptyAImg}
+                width={107}
+                height={60}
+                style={{
+                  borderRadius: "4px",
+                }}
+              />
+            </FoodItem>
+          ))}
+        </FoodImageList>
+        <CompleteButton
+          width="335px"
+          height="56px"
+          variant="primary"
+          // onClick={onToggleDrinkSearchModal}
+        >
+          완료
+        </CompleteButton>
+      </Container>
+      {/**
+       * @Note 음식점 검색
+       */}
+      {/* <VoteHeader
         rightButton={
           <CloseButton onClick={onToggleSearchRestaurantModal}>
             <SvgIcX width={24} height={24} />
@@ -47,7 +99,7 @@ function SearchRestaurantModal({ onToggleSearchRestaurantModal }: Props) {
             <RestaurantItem />
           ))}
         </RestaurantList>
-      </Container>
+      </Container> */}
     </ModalTemplate>
   );
 }
@@ -64,12 +116,55 @@ const CloseButton = styled(Button)`
   margin: 14px 12px 0 0;
 `;
 
-const RestaurantList = styled.ol`
+const FoodImageList = styled.ul`
   display: flex;
-  flex-direction: column;
-  margin-top: 8px;
+  flex-wrap: wrap;
+  padding-top: 24px;
+  gap: 7px;
+  border-top: 1px solid ${({ theme }) => theme.colors.line_01};
   overflow: auto;
-  height: 100%;
+  padding-bottom: 80px;
+  -ms-overflow-style: none /* IE and Edge 스크롤바 없애는 css*/;
+  scrollbar-width: none; /* Firefox 스크롤바 없애는 css */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome , Safari , Opera 스크롤바 없애는 css*/
+  }
+`;
+
+const NonSelectedButton = styled(Button)`
+  position: relative;
+`;
+
+const FoodItem = styled.div`
+  position: relative;
+`;
+
+const ColorBox = styled.div<{ theme: ThemeColors; selected: boolean }>`
+  ${({ theme, selected }) =>
+    css`
+      border-radius: 4px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      ${selected &&
+      css`
+        border: 2px solid ${theme.colors.main_01};
+        background: rgba(255, 74, 22, 0.7);
+        animation: ${transitions.blink} 0.7s ease-in-out;
+      `}
+    `}
+`;
+
+const CompleteButton = styled(Button)`
+  ${({ theme }) =>
+    css`
+      ${theme.typography.body01}
+
+      position: absolute;
+      bottom: 54px;
+    `}
 `;
 
 export default SearchRestaurantModal;
