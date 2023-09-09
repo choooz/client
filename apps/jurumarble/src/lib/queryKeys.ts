27ecb0ebd5e67ce1af495fb5c6f75b97597d559d
이재하 <jhl2619@naver.com>
2023-09-09 23:03:37 +0900

    feat: react query key 추가

--- apps/jurumarble/src/lib/queryKeys.ts
+++ apps/jurumarble/src/lib/queryKeys.ts
@@ -1,10 +1,18 @@
 export const queryKeys = {
   MAIN_VOTE_LIST: "mainVoteList" as const,
   BOOKMARK_CHECK: "bookmarkCheck" as const,
+  USER_INFO: "userInfo" as const,
+  VOTE_LIST: "voteList" as const,
+  RESTAURANT_LIST: "restaurantList" as const,
+  DRINK_LIST: "drinkList" as const,
 };
 
 export const reactQueryKeys = {
   // @note any 처리
   mainVoteList: () => [queryKeys.MAIN_VOTE_LIST] as const,
   bookmarkCheck: () => [queryKeys.BOOKMARK_CHECK] as const,
+  userInfo: () => [queryKeys.USER_INFO],
+  voteList: (params: any) => [queryKeys.VOTE_LIST, ...params],
+  restaurantList: (params: any) => [queryKeys.RESTAURANT_LIST, ...params],
+  drinkList: (params: any) => [queryKeys.DRINK_LIST, ...params],
 };
