bd58da36c51dbcee05aedb6c0f4322174d9f1b04
이재하 <jhl2619@naver.com>
2023-09-15 01:44:50 +0900

    feat: 전통주 리스트를 가져오는 api 추가

---
+++ apps/jurumarble/src/lib/apis/drink.ts
@@ -0,0 +1,62 @@
+import { SortType } from "src/types/common";
+import { baseApi } from "./http/base";
+
+export interface GetDrinkListRequest {
+  page: number;
+  size: number;
+  keyword?: string;
+  region?: string;
+  sortBy: SortType;
+}
+
+interface GetDrinkListResponse {
+  sort: Sort;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: Pageable;
+  size: number;
+  content: DrinkInfo[];
+  empty: boolean;
+}
+
+export interface DrinkInfo {
+  id: number;
+  name: string;
+  type: string;
+  productName: string;
+  alcoholicBeverage: string;
+  rawMaterial: string;
+  capacity: string;
+  manufactureAddress: string;
+  region: string;
+  price: string;
+  image: string;
+  latitude: number;
+  longitude: number;
+  enjoyCount: number;
+}
+
+interface Pageable {
+  sort: Sort;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
+  offset: number;
+}
+
+interface Sort {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+export const getDrinkList = async (params: GetDrinkListRequest) => {
+  const response = await baseApi.get<GetDrinkListResponse>("api/drinks", {
+    params: {
+      ...params,
+    },
+  });
+  return response.data;
+};
