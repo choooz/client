8223ac40799b8c713b8f48d4f7d0515071c67b86
이재하 <jhl2619@naver.com>
2023-09-15 01:36:08 +0900

    feat: hook 패키지에 infinite scroll 훅 추가

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,5 @@
 export { default as useToggle } from "./useToggle";
 export { default as useOutsideClick } from "./useOutsideClick";
 export { default as useDebounce } from "./useDebounce";
+export { default as useInfiniteScroll } from "./useInfiniteScroll";
+export { default as useIntersectionObserver } from "./useIntersectionObserver";
