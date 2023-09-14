d1664a06fb3a55581c02a0b717ea156b3faa520c
이재하 <jhl2619@naver.com>
2023-09-15 02:06:14 +0900

    feat: 전통주 투표에서 투표 정보를 보여주는 컴포넌트 구현

---
+++ apps/jurumarble/src/app/search/components/VoteDescription.tsx
@@ -0,0 +1,43 @@
+import Image, { StaticImageData } from "next/image";
+import styled from "styled-components";
+
+interface Props {
+  imageA: string | StaticImageData;
+  imageB: string | StaticImageData;
+}
+
+function VoteDescription({ imageA, imageB }: Props) {
+  return (
+    <Container>
+      <ImageWrapper>
+        <LeftVote>
+          <Image src={imageA} alt="A 이미지" fill />
+        </LeftVote>
+        <RightVote>
+          <Image src={imageB} alt="B 이미지" fill />
+        </RightVote>
+      </ImageWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  gap: 9px;
+`;
+const LeftVote = styled.div`
+  position: relative;
+  width: 50%;
+  aspect-ratio: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const RightVote = styled(LeftVote)``;
+
+export default VoteDescription;
