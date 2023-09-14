ff9c8b29ec4d36e4fcb5f93417483583abe66237
이재하 <jhl2619@naver.com>
2023-09-15 01:50:01 +0900

    feat: 지역명과 몇명이 즐겼는지를 보여주는 컴포넌트 추가

---
+++ apps/jurumarble/src/components/Chip.tsx
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+interface Props {
+  variant: "region" | "numberOfParticipants";
+  children: React.ReactNode;
+}
+
+type variant = Pick<Props, "variant">;
+
+function Chip({ variant, children }: Props) {
+  return <Wrapper variant={variant}>{children}</Wrapper>;
+}
+
+const variantStyles = {
+  region: css`
+    ${({ theme }) => css`
+      background-color: ${theme.colors.main_02};
+      color: ${theme.colors.main_01};
+    `}
+  `,
+  numberOfParticipants: css`
+    ${({ theme }) => css`
+      background-color: ${theme.colors.bg_01};
+      color: ${theme.colors.black_02};
+    `}
+  `,
+};
+
+const Wrapper = styled.div<variant>`
+  ${({ theme, variant }) => css`
+    ${variant && variantStyles[variant]};
+    ${theme.typography.caption}
+    padding: 10px 8px;
+    border-radius: 4px;
+  `}
+`;
+
+export default Chip;
--- apps/jurumarble/src/components/button/Chip.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// TODO: Chip 컴포넌트 구현
-function Chip() {
-  return <div>Chip</div>;
-}
-
-export default Chip;
