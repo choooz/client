f54f265e71e55b94c3f451c6fd5a96f03e7b14eb
이재하 <jhl2619@naver.com>
2023-09-15 02:06:55 +0900

    feat: 검색 페이지 구현

--- apps/jurumarble/src/app/search/page.tsx
+++ apps/jurumarble/src/app/search/page.tsx
@@ -1,67 +1,204 @@
 "use client";
 
 import BottomBar from "components/BottomBar";
+import { Button } from "components/button";
+import SearchInput from "components/SearchInput";
+import useInput from "hooks/useInput";
 import SvgIcPrev from "src/assets/icons/components/IcPrev";
-import SvgIcX from "src/assets/icons/components/IcX";
-import styled, { css } from "styled-components";
+import styled, { css, DefaultTheme } from "styled-components";
+import DrinkList from "./components/DrinkList";
+import DrinkVoteList from "./components/DrinkVoteList";
+import { useState } from "react";
+import { SORT_LIST } from "lib/constants";
+import SortSelect from "./components/SortSelect";
+import RegionSmallSelect from "./components/RegionSmallSelect";
+
+const TAB_LIST = [
+  { id: "total", name: "통합" },
+  { id: "drinkInfo", name: "우리술 정보" },
+  { id: "drinkVote", name: "우리술 투표" },
+];
 
 function Search() {
-  return (
-    <Container>
-      <SearchBox>
-        <SvgIcPrev width={24} height={24} />
+  const { value: searchText, onChange: onChangeSearchText } = useInput({
+    initialValue: "",
+    useDebounce: true,
+  });
 
-        <SearchInput placeholder="관심있는 술을 검색해보세요." />
-        <ResetIconBox>
-          <SvgIcX width={18} height={18} color="#CCCCCC" />
-        </ResetIconBox>
-      </SearchBox>
+  const [selectedTab, setSelectedTab] = useState("total");
+  const onClickSelectedTab = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelectedTab(e.currentTarget.value);
+  };
+
+  const [sortOption, setSortOption] = useState(SORT_LIST[1].value);
+  const onChangeSortOption = (value: string) => {
+    setSortOption(value);
+  };
+
+  const [regionOption, setRegionOption] = useState("");
+  const onChangeRegionOption = (value: string) => {
+    setRegionOption(value);
+  };
+
+  const isSelectedTab = (tabName: string) => {
+    return selectedTab === tabName;
+  };
+
+  return (
+    <>
+      <TopSection>
+        <SearchBox>
+          <SvgIcPrev width={24} height={24} />
+          <SearchInput value={searchText} onChange={onChangeSearchText} />
+        </SearchBox>
+        <TabBox>
+          {TAB_LIST.map(({ id, name }) => (
+            <SelectedButton
+              key={`tab_${id}`}
+              value={id}
+              selected={id === selectedTab}
+              onClick={onClickSelectedTab}
+            >
+              {name}
+            </SelectedButton>
+          ))}
+        </TabBox>
+        {!isSelectedTab("total") && (
+          <SelectContainer>
+            <SortSelect defaultOption={sortOption} onChangeSortOption={onChangeSortOption} />
+            <RegionSmallSelect
+              defaultOption={regionOption}
+              onChangeSortOption={onChangeRegionOption}
+            />
+          </SelectContainer>
+        )}
+        {isSelectedTab("total") && <H2>우리술 정보</H2>}
+        {isSelectedTab("drinkVote") ? (
+          <></>
+        ) : (
+          <>
+            <DrinkList
+              searchText={searchText}
+              sortOption={sortOption}
+              regionOption={regionOption}
+              isSelectedTab={isSelectedTab("total")}
+            />
+            {selectedTab === "total" && (
+              <MoreButton
+                variant="outline"
+                width="100%"
+                height="48px"
+                /**
+                 * @Todo enum으로 사용하면 좋을 것 같다.
+                 */
+                value={"drinkInfo"}
+                onClick={onClickSelectedTab}
+              >
+                우리술 정보 더보기
+              </MoreButton>
+            )}
+          </>
+        )}
+      </TopSection>
+      {isSelectedTab("total") && <DivideLine />}
+      <BottomSection>
+        {selectedTab === "drinkInfo" ? (
+          <></>
+        ) : (
+          <>
+            {isSelectedTab("total") && <H2>우리술 투표</H2>}
+            <DrinkVoteList
+              searchText={searchText}
+              sortOption={sortOption}
+              regionOption={regionOption}
+              isSelectedTab={isSelectedTab("total")}
+            />
+            {isSelectedTab("total") && (
+              <MoreButton
+                variant="outline"
+                width="100%"
+                height="48px"
+                value="drinkVote"
+                onClick={onClickSelectedTab}
+              >
+                우리술 투표 더보기
+              </MoreButton>
+            )}
+          </>
+        )}
+      </BottomSection>
 
       <BottomBar />
-    </Container>
+    </>
   );
 }
 
-const Container = styled.div`
-  height: 100vh;
-  overflow: hidden;
+const TopSection = styled.section`
+  padding: 0 20px;
 `;
 
 const SearchBox = styled.div`
-  position: relative;
-  padding: 8px 20px;
   display: flex;
   align-items: center;
-  gap: 12px;
-  box-shadow: 0px 16px 32px 0px rgba(235, 235, 235, 0.6);
+  gap: 14px;
 `;
 
-const SearchInput = styled.input`
-  flex: 1;
-  border-radius: 8px;
-  border: none;
-  padding: 12px;
-  &:focus {
-    outline: none;
-  }
+const TabBox = styled.ul`
+  display: flex;
+  margin-top: 8px;
+`;
+
+const DivideLine = styled.div`
   ${({ theme }) => css`
-    ${theme.typography.body02};
-    background-color: ${theme.colors.bg_02};
-    color: ${theme.colors.black_01};
-    &::placeholder {
-      color: ${theme.colors.black_05};
-    }
+    background-color: ${theme.colors.bg_01};
+    height: 8px;
   `}
 `;
 
-const ResetIconBox = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 32px;
+const SelectedButton = styled.button<{ theme: DefaultTheme; selected: boolean }>`
+  ${({ theme, selected }) =>
+    css`
+      ${theme.typography.body02};
+      color: ${theme.colors.black_03};
+      padding: 16.5px 12px;
+
+      ${selected
+        ? css`
+            ${theme.typography.body01};
+            color: ${theme.colors.black_01};
+            border-bottom: 3px solid ${theme.colors.black_01};
+          `
+        : css`
+            display: flex;
+          `}
+    `}
+`;
+
+const SelectContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  margin-top: 40px;
+  gap: 4px;
+`;
+
+const H2 = styled.h2`
+  ${({ theme }) => css`
+    ${theme.typography.headline02};
+    color: ${theme.colors.black_01};
+    margin-top: 40px;
+  `}
+`;
+
+const MoreButton = styled(Button)`
+  ${({ theme }) => css`
+    ${theme.typography.body01}
+    margin: 24px 0 40px 0;
+  `};
+`;
+
+const BottomSection = styled.section`
+  padding: 0 20px 96px;
+  margin-top: 8px; // 64(BottomBar height) + 32(margin) = 96
+  overflow: auto;
 `;
 
 export default Search;
