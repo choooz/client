907cdd174e09523a942aefc78ad55826975cc362
이재하 <jhl2619@naver.com>
2023-09-18 01:37:43 +0900

    chore: 아이콘 추가

---
+++ apps/jurumarble/src/assets/icons/components/IcLogo.tsx
@@ -0,0 +1,87 @@
+import type { SVGProps } from "react";
+const SvgLogo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 111 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g id="Group 5">
+      <path
+        id="Vector"
+        d="M25.1721 12.8677H13.2151V15.3855H25.1721V12.8677Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_2"
+        d="M22.6558 0.910706H13.2166V3.42851H22.6558V4.68519H20.138V7.20299H22.6558L23.5994 7.20447C24.4688 7.20447 25.1721 6.49973 25.1721 5.63177V0.910706H22.6543H22.6558Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_3"
+        d="M15.7329 19.7905V16.6436H13.2151V19.7905H15.7329Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_4"
+        d="M25.1721 19.7905V16.6436H22.6543V19.7905H25.1721Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_5"
+        d="M11.3279 0.910706H0V3.42851H11.3279V0.910706Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_6"
+        d="M2.5178 4.70003V7.83356H1.57418C0.704748 7.83356 0 8.53682 0 9.40626V19.7905H2.5178V10.3647H5.03561V4.70003H2.5178Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_7"
+        d="M9.75371 7.83356H8.81009V4.70003H6.29228V10.3632H8.81009V19.789H11.3279V9.40626C11.3279 8.53682 10.6231 7.83356 9.75519 7.83356H9.75371Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_8"
+        d="M22.1914 8.1481C21.5935 8.7119 20.4748 9.09172 19.1944 9.09172C17.2834 9.09172 15.7329 8.24602 15.7329 7.20448H18.2507V4.68668H13.2166V7.20448C13.2166 9.63771 15.8932 11.6095 19.1944 11.6095C22.0564 11.6095 24.4481 10.1273 25.0341 8.1481H22.1914Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_9"
+        d="M50.0579 13.4953H42.6825V19.8706H40.0579V13.4953H32.7656V11.3706H50.0579V13.4953ZM39.8591 3.99528H34.3279V1.91071H48.5163V3.99379H42.9852C43.2567 5.85878 45.3502 7.71338 49.2463 8.18074L48.2878 10.2638C44.954 9.81575 42.5683 8.39884 41.4229 6.45077C40.2671 8.39884 37.9021 9.81575 34.5994 10.2638L33.6202 8.18074C37.454 7.71189 39.5786 5.85729 39.8605 3.99379L39.8591 3.99528Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_10"
+        d="M53.0252 12.2653H70.3798V14.3899H63.0044V19.9107H60.3383V14.3899H53.0252V12.2653ZM68.3798 7.14067H57.7745V8.72376H68.816V10.8484H55.0861V5.14067H65.6499V3.64067H55.0237V1.49379H68.3783V7.13919L68.3798 7.14067Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_11"
+        d="M82.4525 15.2445H73.3487V2.70299H82.4525V15.246V15.2445ZM79.8071 13.1407V4.78608H75.9733V13.1407H79.8071ZM85.0356 0.910706H87.7018V8.05759H90.4941V10.2653H87.7018V19.8706H85.0356V0.910706Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+      <path
+        id="Vector_12"
+        d="M93.4807 8.64067H110.773V10.703H93.4807V8.64067ZM95.4807 11.8276H108.669V16.6199H98.1053V17.7445H109.169V19.7445H95.5015V14.7653H106.045V13.7653H95.4807V11.8276ZM98.3145 2.5561H105.918V1.22375H108.564V7.59913H95.6884V1.22375H98.3131V2.55758L98.3145 2.5561ZM105.918 5.61841V4.47301H98.3145V5.61841H105.918Z"
+        fill="currentColor"
+        vectorEffect="non-scaling-stroke"
+      />
+    </g>
+  </svg>
+);
+export default SvgLogo;
