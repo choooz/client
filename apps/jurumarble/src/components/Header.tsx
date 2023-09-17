9e5b8946e83262d46192abbc878c6101dae228d3
이재하 <jhl2619@naver.com>
2023-09-18 01:39:40 +0900

    feat: 헤더 구현

--- apps/jurumarble/src/components/Header.tsx
+++ apps/jurumarble/src/components/Header.tsx
@@ -1,13 +1,28 @@
+import { SvgHeaderSearch, SvgLogo, SvgNotification } from "src/assets/icons/components";
 import styled from "styled-components";
 
 function Header() {
-  return <Container />;
+  return (
+    <Container>
+      <SvgLogo width={110} height={19} />
+      <IconContainer>
+        <SvgNotification width={24} height={24} />
+        <SvgHeaderSearch width={24} height={24} />
+      </IconContainer>
+    </Container>
+  );
 }
 
-const Container = styled.header`
-  height: 24px;
-  margin-top: 18px;
-  background-color: ${({ theme }) => theme.colors.main_01};
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  gap: 16px;
 `;
 
 export default Header;
