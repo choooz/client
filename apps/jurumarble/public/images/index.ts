cd3b21f8ae9a0dc87580fc45ed425efc508f0b46
이재하 <jhl2619@naver.com>
2023-09-19 19:18:00 +0900

    chore: 메인 배너 이미지 추가

--- apps/jurumarble/public/images/index.ts
+++ apps/jurumarble/public/images/index.ts
@@ -2,3 +2,4 @@ export { default as EmptyAImg } from "./AImg.png";
 export { default as ExImg1 } from "./exe1.png";
 export { default as ExImg2 } from "./exe2.png";
 export { default as DrinkImage } from "./DrinkImage.png";
+export { default as MainBannerImage } from "./MainBannerImage.png";
