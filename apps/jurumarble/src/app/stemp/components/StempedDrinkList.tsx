9ae1eabf8e141c05194469ae40ac47b9156d0f83
이재하 <jhl2619@naver.com>
2023-09-23 16:01:51 +0900

    feat: 즐긴 우리 술 리스트를 보여주는 컴포넌트 추가

---
+++ apps/jurumarble/src/app/stemp/components/StempedDrinkList.tsx
@@ -0,0 +1,44 @@
+import RegionSelect from "components/RegionSelect";
+import Path from "lib/Path";
+import { useRouter } from "next/navigation";
+import { DrinkInfo } from "src/types/drink";
+import styled from "styled-components";
+import DrinkItem from "./DrinkItem";
+
+interface Props {
+  regionOption: string;
+  onChangeRegionOption: (value: string) => void;
+  drinkList: DrinkInfo[];
+}
+
+function StempedDrinkList({ regionOption, onChangeRegionOption, drinkList }: Props) {
+  const router = useRouter();
+
+  return (
+    <Container>
+      <RegionSelect regionOption={regionOption} onChangeRegionOption={onChangeRegionOption} />
+      <DrinkList>
+        {drinkList.map((drink) => (
+          <DrinkItem
+            key={drink.id}
+            drinkInfo={drink}
+            onClickReplaceDrinkInfo={() => router.push(`${Path.DRINK_INFO_PAGE}/${drink.id}`)}
+          />
+        ))}
+      </DrinkList>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  padding: 24px 20px;
+`;
+
+const DrinkList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-top: 24px;
+`;
+
+export default StempedDrinkList;
