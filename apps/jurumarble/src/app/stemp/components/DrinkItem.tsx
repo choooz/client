7a7f1aae0ffc567211ee52eac81e15515f36decc
이재하 <jhl2619@naver.com>
2023-09-23 16:01:03 +0900

    feat: 술도장에서 사용하는DrinkItem 추가

---
+++ apps/jurumarble/src/app/stemp/components/DrinkItem.tsx
@@ -0,0 +1,108 @@
+import { transitions } from "lib/styles";
+import Image from "next/image";
+import SvgStamp from "src/assets/icons/components/IcStamp";
+import { DrinkInfo } from "src/types/drink";
+import styled, { css, useTheme } from "styled-components";
+import useDrinkStempService from "../service/useDrinkStempService";
+
+interface Props {
+  drinkInfo: DrinkInfo;
+  onClickReplaceDrinkInfo: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  selectedDrinkList?: string[];
+}
+
+function DrinkItem({ drinkInfo, onClickReplaceDrinkInfo, selectedDrinkList }: Props) {
+  const { id, name, productName, image } = drinkInfo;
+
+  const { colors } = useTheme();
+
+  const { isStempedDrink, postDrinkEnjoy } = useDrinkStempService(id);
+
+  const stempColor = isStempedDrink?.enjoyed ? colors.main_01 : colors.black_05;
+
+  return (
+    <Container
+      onClick={onClickReplaceDrinkInfo}
+      name={name}
+      selected={selectedDrinkList?.includes(name)}
+    >
+      <ImageWrapper>
+        <Image alt={name} src={image} fill style={{ borderRadius: "10px" }} />
+      </ImageWrapper>
+      <InfoContainer>
+        <NameStampContainer>
+          <Name>{name}</Name>
+          <StampWrapper
+            onClick={(e) => {
+              e.stopPropagation();
+              postDrinkEnjoy(id);
+            }}
+          >
+            <SvgStamp width={24} height={24} fill={stempColor} />
+          </StampWrapper>
+        </NameStampContainer>
+        <ManufacturerName>{productName}</ManufacturerName>
+      </InfoContainer>
+    </Container>
+  );
+}
+
+const Container = styled.button<{ selected: boolean | undefined }>`
+  display: flex;
+  box-shadow: 0px 2px 8px 0px rgba(235, 235, 235, 0.4), 0px 8px 20px 0px rgba(235, 235, 235, 0.4);
+  height: 120px;
+  padding: 16px;
+  border-radius: 16px;
+  cursor: pointer;
+  ${({ theme, selected }) =>
+    selected &&
+    css`
+      animation: ${transitions.blink} 0.7s ease-in-out;
+      border: 1px solid ${theme.colors.black_01};
+    `};
+`;
+
+const ImageWrapper = styled.div`
+  width: 88px;
+  height: 88px;
+  position: relative;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin-left: 12px;
+`;
+
+const NameStampContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StampWrapper = styled.button`
+  margin-right: 1px;
+`;
+
+const Name = styled.div`
+  ${({ theme }) =>
+    css`
+      ${theme.typography.body01}
+      color: ${theme.colors.black_01};
+      display: flex;
+      flex-direction: column;
+    `}
+`;
+
+const ManufacturerName = styled.span`
+  ${({ theme }) =>
+    css`
+      ${theme.typography.body03}
+      color: ${theme.colors.black_03};
+      margin-top: 4px;
+      display: inherit;
+    `}
+`;
+
+export default DrinkItem;
