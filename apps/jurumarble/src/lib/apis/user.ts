2a15b075803cbc3fce98dba8dc55bfa10a705a25
이재하 <jhl2619@naver.com>
2023-09-25 20:03:44 +0900

    feat: 유저 정보를 가져오는 서비스 로직 추가

---
+++ apps/jurumarble/src/lib/apis/user.ts
@@ -0,0 +1,21 @@
+import { GENDER } from "lib/constants";
+import { http } from "./http/http";
+
+type GenderType = keyof typeof GENDER;
+
+export type Agetype = "teenager" | "twenties" | "thirties" | "forties" | "fifties" | null;
+
+export interface GetUserInfoResponse {
+  nickname: string;
+  email: string;
+  imageUrl: string;
+  ageType: Agetype;
+  gender: GenderType;
+  mbti: string;
+  alcoholLimit: string;
+}
+
+export const getUserInfo = async () => {
+  const response = await http.get<GetUserInfoResponse>("api/users");
+  return response.data;
+};
