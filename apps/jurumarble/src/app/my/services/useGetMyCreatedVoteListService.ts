4aab4978d5b0a7907235902294f38b1747c5d942
이재하 <jhl2619@naver.com>
2023-09-25 20:13:49 +0900

    feat: 마이페이지에 사용되는 서비스 로직 추가

---
+++ apps/jurumarble/src/app/my/services/useGetMyCreatedVoteListService.ts
@@ -0,0 +1,36 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteScroll } from "@monorepo/hooks";
+import { getMyCreatedVoteList } from "lib/apis/my";
+import { queryKeys } from "lib/queryKeys";
+
+type GetMyCreatededVoteListServiceProps = Exclude<
+  Parameters<typeof getMyCreatedVoteList>[0],
+  undefined
+>;
+
+const getMyCreatedVoteQueryKey = (params: GetMyCreatededVoteListServiceProps) => [
+  queryKeys.MY_CREATED_VOTE,
+  params,
+];
+
+export default function useGetMyCreatedVoteListService(params: GetMyCreatededVoteListServiceProps) {
+  const { data, fetchNextPage } = useInfiniteQuery(
+    getMyCreatedVoteQueryKey(params),
+    ({ pageParam }) => getMyCreatedVoteList({ ...params, page: pageParam?.page || 0 }),
+    {
+      getNextPageParam: ({ last, number }) => {
+        if (last) return undefined;
+        return {
+          page: number + 1,
+        };
+      },
+      keepPreviousData: true,
+    },
+  );
+
+  const [subscribe] = useInfiniteScroll(fetchNextPage);
+
+  const myVoteList = data?.pages.flatMap((page) => page.content) ?? [];
+
+  return { myVoteList, subscribe };
+}
