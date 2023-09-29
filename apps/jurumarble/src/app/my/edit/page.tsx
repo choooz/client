8df8a1cf62b45444db8db4c774333dd50498ac65
이재하 <jhl2619@naver.com>
2023-09-29 23:54:28 +0900

    feat: 유저 정보를 수정하는 페이지 구현

---
+++ apps/jurumarble/src/app/my/edit/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "components/button";
+import VoteHeader from "components/VoteHeader";
+import { useRouter } from "next/navigation";
+import { SvgIcPrevious } from "src/assets/icons/components";
+import styled, { css } from "styled-components";
+import UserInfoEditContainer from "./components/UserInfoEditContainer";
+
+function ProfileEditPage() {
+  const router = useRouter();
+  /**
+   * @TODO 헤더 픽스드인지 확인
+   */
+  return (
+    <Container>
+      <VoteHeader
+        leftButton={
+          <PreviousButton onClick={() => router.back()}>
+            <SvgIcPrevious width={24} height={24} />
+          </PreviousButton>
+        }
+      >
+        프로필 수정
+      </VoteHeader>
+      <UserInfoEditContainer />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const PreviousButton = styled(Button)`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+  `}
+`;
+
+export default ProfileEditPage;
