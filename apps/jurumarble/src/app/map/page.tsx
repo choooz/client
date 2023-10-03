fed2595e4ed924150fea1cf314984c3201799527
이재하 <jhl2619@naver.com>
2023-10-04 01:28:40 +0900

    fix: 카카오 맵 스크립트 코드를 해당 페이지로 이동시켜 배포 에러 해결

--- apps/jurumarble/src/app/map/page.tsx
+++ apps/jurumarble/src/app/map/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import BottomBar from "components/BottomBar";
 import Header from "components/Header";
 import React from "react";
 import MapContainer from "./components/MapContainer";
+import { KAKAO_MAP_API_KEY } from "lib/constants";
+import Script from "next/script";
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 const MapPage = () => {
   return (
     <>
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
+      ></Script>
       <Header />
       <MapContainer />
       <BottomBar />
