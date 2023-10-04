4a6853620fbe6ad2d950610967dc9685b8af4df5
이재하 <jhl2619@naver.com>
2023-10-04 22:19:54 +0900

    feat: 헤더에 링크 연결

--- apps/jurumarble/src/components/Header.tsx
+++ apps/jurumarble/src/components/Header.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import Path from "lib/Path";
+import Link from "next/link";
 import { SvgHeaderSearch, SvgLogo, SvgNotification } from "src/assets/icons/components";
 import styled from "styled-components";
 
 function Header() {
   return (
     <Container>
-      <SvgLogo width={110} height={19} />
+      <Link href={Path.MAIN_PAGE}>
+        <SvgLogo width={110} height={19} />
+      </Link>
       <IconContainer>
-        <SvgNotification width={24} height={24} />
-        <SvgHeaderSearch width={24} height={24} />
+        <Link href={Path.NOTIFICATION_PAGE}>
+          <SvgNotification width={24} height={24} />
+        </Link>
+        <Link href={Path.SEARCH_PAGE}>
+          <SvgHeaderSearch width={24} height={24} />
+        </Link>
       </IconContainer>
     </Container>
   );
