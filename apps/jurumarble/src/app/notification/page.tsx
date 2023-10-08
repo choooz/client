26ec0dffccfa481aa1cc08278f6530a2d8daaf1f
이재하 <jhl2619@naver.com>
2023-10-08 19:41:07 +0900

    feat: 알림 구현

--- apps/jurumarble/src/app/notification/page.tsx
+++ apps/jurumarble/src/app/notification/page.tsx
@@ -2,21 +2,23 @@
 
 import { Button } from "components/button";
 import VoteHeader from "components/VoteHeader";
+import { NotificationType } from "lib/apis/notification";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { DrinkImage } from "public/images";
 import { SvgIcPrevious, SvgNotificationCheck } from "src/assets/icons/components";
 import styled, { css, useTheme } from "styled-components";
 import useNotificationService from "./services/useNotificationService";
 
-const NOTIFICATION_TYPE = {
-  VOTE: "VOTE",
-  COMMENT: "투표에 댓글이 달렸어요",
+const NOTIFICATION_TYPE: Record<NotificationType, string> = {
+  VOTE: "투표에 10명 이상이 참여했어요.",
+  COMMENT: "투표에 댓글이 달렸어요.",
+  ADMIN_NOTIFY: "관리자 알림",
 };
-
-// const eventSource = new EventSource("/api/notification/subscribe");
 
 function NotificationPage() {
   const router = useRouter();
-  const { notificationList } = useNotificationService();
+  const { notificationList, isLoading, readNotification } = useNotificationService();
   const { colors } = useTheme();
 
   return (
@@ -30,17 +32,45 @@ function NotificationPage() {
       >
         알림
       </VoteHeader>
-      <NotificationItem>
-        <SvgNotificationCheck width={32} height={32} fill={colors.black_04} />
-        <ContentBox>
-          <Content>부산에놀러가는데술추천좀해주</Content>
-          <TypeMessage>투표에 댓글이 달렸어요.</TypeMessage>
-          <CreatedAt>1분전</CreatedAt>
-        </ContentBox>
-      </NotificationItem>
+      {isLoading ? (
+        /**
+         * @TODO 로딩 컴포넌트 추가
+         */
+        <></>
+      ) : !notificationList ? (
+        <EmptyNotification>
+          <Image alt="" src={DrinkImage} style={{ borderRadius: "100px" }} />
+          받은 알림이 없어요.
+        </EmptyNotification>
+      ) : (
+        <NotificationList>
+          {notificationList.map(({ content, createdAt, id, type, isRead }) => (
+            <NotificationItem key={id} isRead={isRead} onClick={() => readNotification(id)}>
+              <SvgNotificationCheck
+                width={32}
+                height={32}
+                fill={isRead ? colors.black_04 : colors.main_01}
+              />
+              <ContentBox>
+                <Content>{content}</Content>
+                <TypeMessage>{NOTIFICATION_TYPE[type]}</TypeMessage>
+                <CreatedAt>{createdAt}</CreatedAt>
+              </ContentBox>
+            </NotificationItem>
+          ))}
+        </NotificationList>
+      )}
     </>
   );
 }
+
+const EmptyNotification = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 22px;
+  margin-top: 16%;
+`;
 
 const PreviousButton = styled(Button)`
   ${({ theme }) => css`
@@ -49,12 +79,18 @@ const PreviousButton = styled(Button)`
   `}
 `;
 
-const NotificationItem = styled.div`
-  ${({ theme }) => css`
-    color: ${theme.colors.black_02};
-    display: flex;
-    padding: 16px 20px;
+const NotificationList = styled.ul`
+  margin-top: 20px;
+`;
+
+const NotificationItem = styled.li<{ isRead: any }>`
+  ${({ theme, isRead }) => css`
+  background-color: ${isRead && theme.colors.bg_02};
+    color: ${isRead ? theme.colors.black_03 : theme.colors.black_02};
     border-top: 1px solid ${theme.colors.line_01}};
+    display: flex;
+    align-items: center;
+    padding: 16px 20px;
   `}
 `;
 
@@ -79,7 +115,7 @@ const TypeMessage = styled.div`
 
 const CreatedAt = styled.div`
   ${({ theme }) => css`
-    ${theme.typography.caption};
+    ${theme.typography.caption_chip};
     color: ${theme.colors.black_04};
     display: flex;
     width: 100%;
