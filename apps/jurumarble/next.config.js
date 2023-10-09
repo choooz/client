85ddc20630d960adb3f1ec21fcfbc6a296adbd72
leejiho9898 <cctv1998@naver.com>
2023-10-09 19:11:38 +0900

    feat: 허용 도매인 추가

--- apps/jurumarble/next.config.js
+++ apps/jurumarble/next.config.js
@@ -9,6 +9,15 @@ const nextConfig = {
     domains: [
       "shopping-phinf.pstatic.net",
       "elasticbeanstalk-ap-northeast-2-319210348301.s3.ap-northeast-2.amazonaws.com",
+      "img.danawa.com",
+      "sulsulsul.com",
+      "shop-phinf.pstatic.net",
+      "www.sulseam.com",
+      "mblogthumb-phinf.pstatic.net",
+      "sogoodk.com",
+      "modo-phinf.pstatic.net",
+      "cdn-pro-web-251-115.cdn-nhncommerce.com",
+      "lh3.googleusercontent.com",
     ],
   },
 };
