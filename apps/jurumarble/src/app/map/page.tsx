ac88e9db79b7803ef7a7e4aa819f2527c84815fc
leejiho9898 <cctv1998@naver.com>
2023-10-09 19:11:52 +0900

    feat: 지도 스크립트 메인으로 이동

--- apps/jurumarble/src/app/map/page.tsx
+++ apps/jurumarble/src/app/map/page.tsx
@@ -4,8 +4,6 @@ import BottomBar from "components/BottomBar";
 import Header from "components/Header";
 import React from "react";
 import MapContainer from "./components/MapContainer";
-import { KAKAO_MAP_API_KEY } from "lib/constants";
-import Script from "next/script";
 
 declare global {
   interface Window {
@@ -15,11 +13,6 @@ declare global {
 const MapPage = () => {
   return (
     <>
-      <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
-      <Script
-        type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
-      ></Script>
       <Header />
       <MapContainer />
       <BottomBar />
