ddd5461d90ec5f18f886e3b04670a316797153a7
이재하 <jhl2619@naver.com>
2023-10-12 01:18:03 +0900

    feat: 말풍선 툴팁 구현

--- apps/jurumarble/src/app/vote/post/components/PostVoteContainer.tsx
+++ apps/jurumarble/src/app/vote/post/components/PostVoteContainer.tsx
@@ -7,7 +7,7 @@ import { Button, Input } from "components/index";
 import VoteHeader from "components/VoteHeader";
 import SvgIcPrevious from "src/assets/icons/components/IcPrevious";
 import { useRouter } from "next/navigation";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import Image from "next/image";
 import TitleAndDescriptionSection from "./TitleAndDescriptionSection";
 import DrinkSearchModal from "./DrinkSearchModal";
@@ -15,6 +15,7 @@ import usePostVoteService from "../services/usePostVoteService";
 import { DrinkInfoType } from "src/types/vote";
 import AorBMark from "components/AorBMark";
 import { media } from "lib/styles";
+import { SvgIcX, SvgInfo } from "src/assets/icons/components";
 
 function PostVoteContainer() {
   const [isDrinkSearchModal, onToggleDrinkSearchModal] = useToggle();
@@ -39,6 +40,8 @@ function PostVoteContainer() {
 
   const { title, detail, titleA, titleB, imageA, imageB } = postVoteInfo;
 
+  const [isTooltip, onToggleTooltip] = useToggle(true);
+
   return (
     <Container>
       <VoteHeader
@@ -55,7 +58,23 @@ function PostVoteContainer() {
           <GuideText>
             고민하고 계신 후보 2개와 <Br /> 후보 이미지를 등록해주세요.
           </GuideText>
-          <SubText>후보 이미지는 선택 사항이에요.</SubText>
+          <SubText>
+            후보 이미지는 선택 사항이에요.
+            <button onClick={onToggleTooltip}>
+              <SvgInfo width={24} height={24} />
+            </button>
+          </SubText>
+          {isTooltip && (
+            <Ballon>
+              <BalloonText>
+                술 검색하기 버튼을 클릭하면 <br />
+                우리술을 편하게 등록할 수 있어요.
+                <button onClick={onToggleTooltip}>
+                  <SvgIcX width={16} height={16} />
+                </button>
+              </BalloonText>
+            </Ballon>
+          )}
         </div>
         <ButtonStyled
           width="96px"
@@ -185,6 +204,9 @@ const SubText = styled.div`
       ${theme.typography.body03}
       color: ${theme.colors.black_03};
       margin-top: 11px;
+      display: flex;
+      align-items: center;
+      gap: 4px;
     `}
 `;
 
@@ -243,6 +265,39 @@ const InputBox = styled.div`
   display: flex;
   gap: 12px;
   flex: 0.5;
+`;
+
+const Ballon = styled.div`
+  position: relative;
+`;
+
+const BalloonText = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.body_long03}
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.system_black};
+    border-radius: 8px;
+    padding: 12px 16px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    gap: 4px;
+    position: absolute;
+    width: 272px;
+    margin-top: 6px;
+    left: 48px;
+
+    ::after {
+      content: "";
+      position: absolute;
+      border-top: 8px solid transparent;
+      border-bottom: 8px solid ${theme.colors.system_black};
+      border-right: 6px solid transparent;
+      border-left: 6px solid transparent;
+      bottom: 100%;
+      left: 49%;
+    }
+  `}
 `;
 
 export default PostVoteContainer;
