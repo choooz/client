90a5cb2b2774c59637cc013a552cadd0d864a9a1
이재하 <jhl2619@naver.com>
2023-10-13 22:24:57 +0900

    feat: 타입가드 추가

--- apps/jurumarble/src/lib/utils/auth.ts
+++ apps/jurumarble/src/lib/utils/auth.ts
@@ -2,14 +2,18 @@ import Path from "lib/Path";
 import userStorage from "./userStorage";
 
 export function logout() {
-  userStorage.remove();
-  window.location.replace(Path.MAIN_PAGE);
+  if (typeof window !== "undefined") {
+    userStorage.remove();
+    window.location.replace(Path.MAIN_PAGE);
+  }
 }
 
 export function isLogin() {
-  const user = userStorage.get();
-  if (!user) return false;
+  if (typeof window !== "undefined") {
+    const user = userStorage.get();
+    if (!user) return false;
 
-  const { accessToken } = user;
-  return !!accessToken;
+    const { accessToken } = user;
+    return !!accessToken;
+  }
 }
