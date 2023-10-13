33409f8278f77a8a24532acf0ebf26770dc26675
이재하 <jhl2619@naver.com>
2023-10-14 02:10:34 +0900

    feat: test 계정의 토큰을 받는 api 추가

--- apps/jurumarble/src/services/useGetUserInfo.ts
+++ apps/jurumarble/src/services/useGetUserInfo.ts
@@ -1,11 +1,11 @@
 import { queryKeys } from "lib/queryKeys";
 import { useQuery } from "@tanstack/react-query";
-import { getUserInfo, GetUserInfoResponse } from "lib/apis/user";
+import { getUserInfoAPI, GetUserInfoResponse } from "lib/apis/user";
 
 const getQueryKey = [queryKeys.USER_INFO];
 
 export default function useGetUserInfo() {
-  const { data: userInfo } = useQuery<GetUserInfoResponse>(getQueryKey, getUserInfo, {
+  const { data: userInfo } = useQuery<GetUserInfoResponse>(getQueryKey, getUserInfoAPI, {
     placeholderData: () => ({
       gender: "MALE",
       nickname: "",
