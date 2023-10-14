0f3fce3cf5008bde8f810d25d2dddba766aab821
leejiho9898 <cctv1998@naver.com>
2023-10-14 20:58:33 +0900

    fix: 지도 안뜨던 버그 수정

--- apps/jurumarble/src/app/map/page.tsx
+++ apps/jurumarble/src/app/map/page.tsx
@@ -2,6 +2,8 @@
 
 import BottomBar from "components/BottomBar";
 import Header from "components/Header";
+import { KAKAO_MAP_API_KEY } from "lib/constants";
+import Script from "next/script";
 import React from "react";
 import MapContainer from "./components/MapContainer";
 
@@ -13,6 +15,11 @@ declare global {
 const MapPage = () => {
   return (
     <>
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
+      ></Script>
       <Header />
       <MapContainer />
       <BottomBar />
--- apps/jurumarble/src/app/page.tsx
+++ apps/jurumarble/src/app/page.tsx
@@ -15,12 +15,6 @@ import SearchInputWrapper from "./main/components/SearchInputWrapper";
 function MainPage() {
   return (
     <>
-      <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
-      <Script
-        type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
-      ></Script>
-
       <Header />
 
       <TopSection>
