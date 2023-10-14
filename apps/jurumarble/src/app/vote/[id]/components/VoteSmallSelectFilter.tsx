9077260c3f279f689440eb14a00a13f8452eab34
leejiho9898 <cctv1998@naver.com>
2023-10-14 16:04:03 +0900

    feat: 필터추가

---
+++ apps/jurumarble/src/app/vote/[id]/components/VoteSmallSelectFilter.tsx
@@ -0,0 +1,59 @@
+import { REGION_LIST } from "lib/constants";
+import styled, { css } from "styled-components";
+import { useToggle } from "@monorepo/hooks";
+import SvgIcExpandMore from "src/assets/icons/components/IcExpandMore";
+import { Select } from "components/selectBox";
+
+interface Props {
+  defaultOption: string;
+  onChangeSortOption: (id: string) => void;
+  options: { value: string; label: string }[];
+}
+
+function VoteSmallSelectFilter({ defaultOption, onChangeSortOption, options }: Props) {
+  const [isOpen, onToggleOpen] = useToggle();
+
+  return (
+    <SelectStyled isOpen={isOpen}>
+      <Select
+        defaultValue={defaultOption}
+        onChangeSelectedOption={onChangeSortOption}
+        options={[...options]}
+        isOpen={isOpen}
+        onToggleOpen={onToggleOpen}
+      >
+        <SvgIcExpandMore width={20} height={20} />
+      </Select>
+    </SelectStyled>
+  );
+}
+
+const SelectStyled = styled.span<{ isOpen: boolean }>`
+  ${({ theme, isOpen }) => css`
+    ${theme.typography.button01};
+    color: ${theme.colors.black_03};
+    width: 85px;
+    height: 40px;
+    .selected-label {
+      border: 1px solid ${theme.colors.line_01};
+      border-radius: 8px;
+      padding: 10px 12px;
+    }
+    svg {
+      ${isOpen && "transform: rotateX( 180deg )"}
+    }
+  `}
+  #select-list {
+    width: 85px;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 8px 0;
+  }
+  #indicator {
+    display: flex;
+  }
+`;
+
+export default VoteSmallSelectFilter;
