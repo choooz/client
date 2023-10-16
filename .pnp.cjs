6a857c705c765186bb49cf2a4e08913290f9a603
이도현 <64634992+ksmfou98@users.noreply.github.com>
2023-10-16 16:41:20 +0900

    fix: axios debug 디팬던시 이슈 수정 (#178)
    * [CZ-293] 메인페이지 투표하기 기능 추가 (#43)
    
    * feat: 페이지 레이아웃 디자인 추가
    
    * fix: transion 슬라이드 효과 수정
    
    * feat: 카드 플립 애니메이션
    
    * refactor: 메인 페이지 코드 분리
    
    * fix: 코드리뷰 반영
    
    * feat: 투표하기 퍼블리싱
    
    * feat: 디테일페이지 이동 아이콘 추가
    
    * feat: 자세히 보기 버튼 생성
    
    * fix: 디자인에 맞게 날짜 폰트 수정
    
    * refactor: dom 조작하는 코드 삭제
    
    * [CZ-513] 댓글 입력시 프로필 사진을 사용하도록 수정 (#100)
    
    * fix: axios debug 디팬던시 이슈 수정
    
    ---------
    
    Co-authored-by: leejha <jhl2619@naver.com>
    Co-authored-by: leejiho9898 <84016039+leejiho9898@users.noreply.github.com>
    Co-authored-by: Leejha <57664427+Leejha@users.noreply.github.com>

--- .pnp.cjs
+++ .pnp.cjs
@@ -6354,11 +6354,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["debug", "virtual:5daecec5378cca9d2b655781dda98457b520644332776e02f43c02749432d960b0fe724013f680814901d9397550da22c63b2a93b9f47877762783265d253473#npm:4.3.4"],\
             ["@types/supports-color", null],\
             ["ms", "npm:2.1.2"],\
-            ["supports-color", null]\
+            ["supports-color", "npm:9.4.0"]\
           ],\
           "packagePeers": [\
-            "@types/supports-color",\
-            "supports-color"\
+            "@types/supports-color"\
           ],\
           "linkType": "HARD"\
         }]\
@@ -6685,11 +6684,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["follow-redirects", "virtual:35a845e191d5a8a7376e256c81e9e855732385142c5dc9e26f0b8393c2fb4673b81fed201d5f2c5ab96fb727142683ac03329eab760f2c4870779d4bda6abe8f#npm:1.15.2"],\
             ["@types/debug", null],\
-            ["debug", null]\
+            ["debug", "virtual:5daecec5378cca9d2b655781dda98457b520644332776e02f43c02749432d960b0fe724013f680814901d9397550da22c63b2a93b9f47877762783265d253473#npm:4.3.4"]\
           ],\
           "packagePeers": [\
-            "@types/debug",\
-            "debug"\
+            "@types/debug"\
           ],\
           "linkType": "HARD"\
         }]\
@@ -8909,6 +8907,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["supports-color", "npm:7.2.0"],\
             ["has-flag", "npm:4.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }],\
+        ["npm:9.4.0", {\
+          "packageLocation": "./.yarn/cache/supports-color-npm-9.4.0-a415f39758-cb8ff8daea.zip/node_modules/supports-color/",\
+          "packageDependencies": [\
+            ["supports-color", "npm:9.4.0"]\
           ],\
           "linkType": "HARD"\
         }]\
