c987b3db051a3c3c8bcbf8ac1d00415147e1bb95
이재하 <jhl2619@naver.com>
2023-10-21 00:16:33 +0900

    style: 메인 배너 이미지 변경

--- apps/jurumarble/src/app/main/components/Banner.tsx
+++ apps/jurumarble/src/app/main/components/Banner.tsx
@@ -6,8 +6,8 @@ import Path from 'lib/Path';
 import userStorage from 'lib/utils/userStorage';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { MainBannerImage } from 'public/images';
-import styled, { css } from 'styled-components';
+import { mainBanner } from 'public/images';
+import styled from 'styled-components';
 
 function Banner() {
   const router = useRouter();
@@ -22,16 +22,10 @@ function Banner() {
     <Container>
       <Image
         alt="배너"
-        src={MainBannerImage}
+        src={mainBanner}
         fill
         style={{ borderRadius: '16px' }}
       />
-      <BannerText>
-        <MainText>여행의 즐거움을 우리술과 함께 레벨업!</MainText>
-        <SubText>
-          여행지에서 우리술이 고민된다면 <br /> 주루마블에서 해결해보세요
-        </SubText>
-      </BannerText>
     </Container>
   );
 }
@@ -42,25 +36,4 @@ const Container = styled.div`
   aspect-ratio: 16 / 9;
 `;
 
-const BannerText = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  height: 100%;
-  padding: 20px;
-`;
-
-const MainText = styled.div`
-  ${({ theme }) => css`
-    ${theme.typography.headline03}
-  `};
-`;
-
-const SubText = styled.div`
-  ${({ theme }) => css`
-    ${theme.typography.body_long03}
-    margin-top: 8px;
-  `};
-`;
 export default Banner;
