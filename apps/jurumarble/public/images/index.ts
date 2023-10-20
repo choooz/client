f1756b61d7976b657fe9a573ae8268c8a950be96
이재하 <jhl2619@naver.com>
2023-10-21 00:16:33 +0900

    style: 메인 배너 이미지 변경

--- apps/jurumarble/public/images/index.ts
+++ apps/jurumarble/public/images/index.ts
@@ -2,7 +2,7 @@ export { default as EmptyAImg } from './AImg.png';
 export { default as ExImg1 } from './exe1.png';
 export { default as ExImg2 } from './exe2.png';
 export { default as DrinkImage } from './DrinkImage.png';
-export { default as MainBannerImage } from './MainBannerImage.png';
+export { default as mainBanner } from './mainBanner.png';
 export { default as Male } from './male.png';
 export { default as Female } from './female.png';
 export { default as DrinkCapacityLow } from './DrinkCapacityLow.png';
