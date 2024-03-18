9514bbd7421f68d2943a037296c82b6c3f941bca
이재하 <jhl2619@naver.com>
2024-03-18 11:06:25 +0900

    feat: 이미지 url을 확인하여 대체 이미지를 반환하는 함수 추가

---
+++ apps/jurumarble/src/lib/utils/classifyImageUrl.ts
@@ -0,0 +1,15 @@
+/**
+ * @TODO DrinkImage 의존성 제거
+ */
+
+import { DrinkImage } from 'public/images';
+
+export function classifyImageUrl(imageUrl: string | null) {
+  const regex =
+    /elasticbeanstalk-ap-northeast-2-319210348301\.s3\.ap-northeast-2\.amazonaws\.com/;
+  if (imageUrl && regex.test(imageUrl)) {
+    return DrinkImage;
+  } else {
+    return imageUrl;
+  }
+}
